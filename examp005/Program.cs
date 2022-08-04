181b6f51fea626e75c9e4439fb9cda8590c6c1b8
Nadin <ppoit@yandex.ru>
2022-08-04 11:53:41 +0300

    add name

---
+++ examp005/Program.cs
@@ -0,0 +1,12 @@
+﻿string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+}
+
